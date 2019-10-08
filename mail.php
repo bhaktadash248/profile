19f4610cd992e923bf4adbc6f5225c10cbe0a201
dashlucky248 <dashlucky248@gmail.com>
2019-10-08 12:49:56 +0530

    added mail function:

---
+++ mail.php
@@ -0,0 +1,9 @@
+<?php
+$to = "dashlucky248@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+mail($to,$subject,$txt,$headers);
+?>
